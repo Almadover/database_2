db3e5dcc4ac71ca39d9076d412e3022fa913d93a
Sergei <serge-koleso@yandex.ru>
2024-02-23 10:24:08 +0300

    initial commit

---
+++ m2m-relations/articles/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+class Article(models.Model):
+
+    title = models.CharField(max_length=256, verbose_name='Название')
+    text = models.TextField(verbose_name='Текст')
+    published_at = models.DateTimeField(verbose_name='Дата публикации')
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    scope = models.ManyToManyField(Tag, through='Scoping')
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
+
+    def __str__(self):
+        return self.title
+
+class Scoping(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, verbose_name='Раздел')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    main = models.BooleanField(default=False, verbose_name='Основной')
+
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = 'Тематики Статьи'
+        ordering = ['-main', 'tag__name']
